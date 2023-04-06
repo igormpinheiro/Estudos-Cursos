fc9708d9877d256de18aabaf7f9f577381166c4c
Igor Martins Pinheiro <igordf@gmail.com>
2023-04-06 20:19:37 +0000

    add componentsand routes

--- Angular/ProjetoDevIO/src/app/app.module.ts
+++ Angular/ProjetoDevIO/src/app/app.module.ts
@@ -2,13 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './navegacao/menu/menu.component';
+import { HomeComponent } from './navegacao/home/home.component';
+import { FooterComponent } from './navegacao/footer/footer.component';
+import { SobreComponent } from './institucional/sobre/sobre.component';
+import { ContatoComponent } from './institucional/contato/contato.component';
+import { RouterModule } from '@angular/router';
+import { rootRouterConfig } from './app.routes';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    HomeComponent,
+    FooterComponent,
+    SobreComponent,
+    ContatoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    [RouterModule.forRoot(rootRouterConfig)]
   ],
   providers: [],
   bootstrap: [AppComponent]
