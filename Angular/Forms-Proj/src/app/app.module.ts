acd140150bdf57bbe29c646f04cc867926196247
Igor Martins Pinheiro <igordf@gmail.com>
2023-04-11 12:26:07 +0000

    Add Forms Proj

---
+++ Angular/Forms-Proj/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MenuComponent } from './navegacao/menu/menu.component';
+import { FooterComponent } from './navegacao/footer/footer.component';
+import { HomeComponent } from './navegacao/home/home.component';
+import { SobreComponent } from './institucional/sobre/sobre.component';
+import { FormsModule } from '@angular/forms';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MenuComponent,
+    FooterComponent,
+    HomeComponent,
+    SobreComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Angular/Forms-Proj/src/app/institucional/sobre/sobre.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sobre',
+  templateUrl: './sobre.component.html',
+  styleUrls: []
+})
+export class SobreComponent {
+
+}
