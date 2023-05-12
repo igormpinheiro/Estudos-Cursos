f86c9ba2841c5241e494b69e5de6b0ec68ed67d5
Igor Martins Pinheiro <igordf@gmail.com>
2023-05-12 12:17:22 -0300

    Configura tema e eslint

--- React/igg-timer/src/App.tsx
+++ React/igg-timer/src/App.tsx
@@ -1,5 +1,18 @@
+import { Button } from './components/Button.tsx'
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/themes/default.ts'
+import { GlobalStyle } from './styles/global.ts'
+
 export function App() {
   return (
-    <div>Ola Mundo</div>
+    <ThemeProvider theme={defaultTheme}>
+      <Button variant="primary" />
+      <Button variant="secondary" />
+      <Button variant="danger" />
+      <Button variant="success" />
+      <Button />
+      lEGAL
+      <GlobalStyle />
+    </ThemeProvider>
   )
 }
