799e7e11b8ea3dbea035668e63ef12e5e2590158
Igor Martins Pinheiro <igordf@gmail.com>
2023-05-13 10:01:19 -0300

    Finaliza History e Status

--- React/igg-timer/src/Router.tsx
+++ React/igg-timer/src/Router.tsx
@@ -1,6 +1,6 @@
 import { Route, Routes } from 'react-router-dom'
 import { Home } from './pages/Home/index.tsx'
-import { History } from './pages/History.tsx'
+import { History } from './pages/History'
 import { Index } from './layouts/DefaultLayout/Index.tsx'
 
 export function Router() {
